99e290ea784f5d642df5cdbd2971e36996126dbd
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-18 22:52:15 +0800

    Added grid function and change grid size function

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// Initialize sketch grid and boxes to be sketched on
-const sketchGrid = document.querySelector('.sketch-grid');
-const sketchBox = document.createElement("div");
+// Create grid based on parameter size
+function createGrid(size){
+    let grid = document.querySelector('.grid');
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    for (let i = 0; i < size*size; i++) {
+        let gridElement = document.createElement('div');
+        gridElement.style.backgroundColor = 'blue';
+        grid.insertAdjacentElement('beforeend', gridElement);
+    }
+}
 
-// Sketch box properties
-sketchBox.style.height = "10px";
-sketchBox.style.width ="10px";
-sketchBox.style.border = "1px solid black";
+// Change grid size based on input from form
+function changeSize(input){
+    if(input => 2 && input <= 100) createGrid(input);
+    else
+        console.log("error");
+}
