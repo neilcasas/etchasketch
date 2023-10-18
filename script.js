ea929f0e987ebe93deefe0247f180141bd65f7d1
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-18 20:34:30 +0800

    Added buttons and interactive container

---
+++ script.js
@@ -0,0 +1,8 @@
+const sketchGrid = document.querySelector('.sketch-grid');
+
+for(columns = 0; columns <= 16; columns++) {
+    for(sketchBoxes = 0; sketchBoxes <= 16; sketchBoxes++)
+    {
+        
+    }
+}
